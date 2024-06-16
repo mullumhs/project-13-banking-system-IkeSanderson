6b01a308928608f6a893f2b01551e34ed80ac748
IkeSanderson <140124177+IkeSanderson@users.noreply.github.com>
2024-06-17 09:50:37 +1000

    Added Transfer

--- bank.py
+++ bank.py
@@ -12,36 +12,64 @@ class BankManager:
     def add_account(self, account):
         self.accounts.append(account)
 
-    def deposite(self, account, amount):
-        for accounts in self.accounts:
-            if accounts.get_number() == account:
-                account.deposite(amount)
-            else:
-                print("Account Not Found")
-    
-    def withdraw(self, account, amount):
-        for accounts in self.accounts:
-            if accounts.get_number() == account:
-                try:
-                    account.withdraw(amount)
-
-                except:
-                    print("Balance Too Low")
-            else:
-                print("Account Not Found")
-
-
-    def search_account(self, account):
-        for accounts in self.accounts:
-            if accounts.get_number() == account:
-                print(account)
+    def deposite(self, account_num, amount):
+        account = self.check_account(account_num)
+        if account:
+            account.deposite(amount)
+            return True
             
-a1 = Account(1234,"John", 100)
-bank = BankManager()
+        print("Account Not Found")
+        return False
+    
+    def withdraw(self, account_num, amount):
+        account = self.check_account(account_num)
+        if account:
+            try:
+                account.withdraw(amount)
+                return True
+                
+            except:
+                print("Balance Too Low")
+                return False
+                
+                
+            
+              
+        print("Account Not Found")
+        return False
 
+
+    def search_account(self, account_num):
+        account = self.check_account(account_num)
+        if account:
+            print(account)
+    
+    def check_account(self, account):
+        for accounts in self.accounts:
+            if accounts.get_number() == account:
+                return accounts
+        print("Account Not Found")
+        return False
+    
+    def transfer(self, account_num1, account_num2, amount ):
+        account1 = self.check_account(account_num1)
+        account2 = self.check_account(account_num2)
+        if account1 and account2:        
+            account1.withdraw(amount)
+            account2.deposite(amount)
+            print("Tranfer Successful")
+                        
+                       
+                        
+
+            
+a1 = Account(1234,"John", 200)
+a2 = Account(4321,"Mike", 800)
+bank = BankManager()
+bank.add_account(a2)
 bank.add_account(a1)
+
+bank.transfer(1234, 4321, 10)
 bank.search_account(1234)
-bank.deposite(1234, 100) # <---- Error 
-bank.search_account(1234)
-bank.withdraw(1234, 500)
-bank.search_account(1234)
+bank.search_account(4321)
+
